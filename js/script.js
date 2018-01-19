d9abe67343a266265cb4a562a68ceec9872a3992
Moh <mohakemal9@gmail.com>
2018-01-19 22:11:28 +0000

    :elephant:

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+
 $(document).ready(function() {
 
 $(function(){
       $(".element").typed({
-        strings: ["<span>Welcome To Mohammed's Portfolio!</span>"],
+        strings: ["<span>:) Welcome To Mohammed's Portfolio!</span>"],
         typeSpeed: 0,
         contentType: "html"
       });
